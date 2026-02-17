86da8326c81025b052b006921afdaef94959e3d2
Arina Mirzakhani <Arina.Mirzakhani@georgebrown.ca>
2026-02-17 08:54:54 -0500

    Lab 05: GraphQL movie queries and mutations

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,3 +1,38 @@
 import MovieModel from '../models/movie.js';
 
-// Resolvers define the technique for fetching the types defined in the schema.
+const resolvers = {
+  Query: {
+    getAllMovies: async () => {
+      return await MovieModel.find();
+    },
+
+    getMovieById: async (_, { id }) => {
+      return await MovieModel.findById(id);
+    },
+
+    getMoviesByDirector: async (_, { director_name }) => {
+      return await MovieModel.findByDirector(director_name); // STATIC method ✅
+    },
+  },
+
+  Mutation: {
+    addMovie: async (_, { input }) => {
+      const movie = new MovieModel(input);
+      return await movie.save();
+    },
+
+    updateMovie: async (_, { id, input }) => {
+      return await MovieModel.findByIdAndUpdate(id, input, {
+        new: true,
+        runValidators: true,
+      });
+    },
+
+    deleteMovieById: async (_, { id }) => {
+      const deleted = await MovieModel.findByIdAndDelete(id);
+      return deleted ? true : false;
+    },
+  },
+};
+
+export default resolvers;
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,1 +1,42 @@
 import { gql } from 'graphql-tag';
+
+const typeDefs = gql`
+  type Movie {
+    id: ID!
+    name: String!
+    director_name: String!
+    production_house: String!
+    release_date: String!
+    rating: Float!
+  }
+
+  input AddMovieInput {
+    name: String!
+    director_name: String!
+    production_house: String!
+    release_date: String!
+    rating: Float!
+  }
+
+  input UpdateMovieInput {
+    name: String
+    director_name: String
+    production_house: String
+    release_date: String
+    rating: Float
+  }
+
+  type Query {
+    getAllMovies: [Movie!]!
+    getMovieById(id: ID!): Movie
+    getMoviesByDirector(director_name: String!): [Movie!]!
+  }
+
+  type Mutation {
+    addMovie(input: AddMovieInput!): Movie!
+    updateMovie(id: ID!, input: UpdateMovieInput!): Movie
+    deleteMovieById(id: ID!): Boolean!
+  }
+`;
+
+export default typeDefs;
